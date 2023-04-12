091f920e2402649216fa8934641223230a7804e2
rollo <rollovitch.roll@yandex.ru>
2023-04-12 14:38:46 +0300

    refactor

--- src/main/java/com/cookbook/cookbook/controllers/v1/ReceiptController.java
+++ src/main/java/com/cookbook/cookbook/controllers/v1/ReceiptController.java
@@ -4,6 +4,7 @@ package com.cookbook.cookbook.controllers.v1;
 import com.cookbook.cookbook.models.receipts.ReceiptPhotoNameShortDescriptionDto;
 import com.cookbook.cookbook.models.receipts.ReceiptDto;
 import com.cookbook.cookbook.services.ReceiptService;
+import org.hibernate.annotations.Parameter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,9 +24,9 @@ public class ReceiptController {
     }
 
     @ResponseBody
-    @GetMapping("/api/v1/receipts")
-    public ReceiptDto getReceiptById(@RequestParam Long receipt){
-        return receiptService.getReceiptById(receipt);
+    @GetMapping("/api/v1/receipt")
+    public ReceiptDto getReceiptById(@RequestParam Long id){
+        return receiptService.getReceiptById(id);
     }
 
     @GetMapping("/api/v1/receipts")
